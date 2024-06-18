96a3b8d19986d850f6cb3839e81b6cb67ac0d8a3
unknown <samson.vang@live.com>
2024-06-18 18:29:08 -0400

    added boolean and comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,12 +5,15 @@ var generateBtn = document.querySelector("#generate");
 
 
 //Here is what I learned today, that could be logged into the console.
-var studentName = "Samson"
-var pets = 1
+var studentName = "Samson" //This is a string
+var pets = 1 //This is a integer
 var funActivity = "I love cars"
+var sexyMan = true //This is a boolean
 
-var studentID = studentName + pets + funActivity
+var studentID = studentName + pets + funActivity + sexyMan
 
 //we console logged the studentname, number of pets, and a fun activity
 
 console.log(studentID)
+
+
