2c8619b53b188ec9e071bcbbed427a409f23b27f
unknown <samson.vang@live.com>
2024-06-18 18:23:40 -0400

    added some fun JS of what I learned today

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,4 +4,13 @@ var generateBtn = document.querySelector("#generate");
 //The generate button is selected by the ID.
 
 
+//Here is what I learned today, that could be logged into the console.
+var studentName = "Samson"
+var pets = 1
+var funActivity = "I love cars"
 
+var studentID = studentName + pets + funActivity
+
+//we console logged the studentname, number of pets, and a fun activity
+
+console.log(studentID)
