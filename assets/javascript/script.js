3758639ced6330ac4f6249f692ad3db5052a5b62
unknown <samson.vang@live.com>
2024-06-18 16:55:54 -0400

    added query selector for the generate button

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+var generateBtn = document.querySelector("#generate");
+//we use the a define variable of generateBtn
+// to create a query selecctor on the generate button.
+//The generate button is selected by the ID.
+
+
+
