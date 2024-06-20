7b07493f1c27b71353ff341502644db4b054fa27
unknown <samson.vang@live.com>
2024-06-19 21:07:29 -0400

    using script.js as a playground

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,36 @@
-var generateBtn = document.querySelector("#generate");
-//we use the a define variable of generateBtn
-// to create a query selecctor on the generate button.
-//The generate button is selected by the ID.
+// var generateBtn = document.querySelector("#generate");
+// //we use the a define variable of generateBtn
+// // to create a query selecctor on the generate button.
+// //The generate button is selected by the ID.
 
 
-//Here is what I learned today, that could be logged into the console.
-var studentName = "Samson" //This is a string
-var pets = 1 //This is a integer
-var funActivity = "I love cars"
-var sexyMan = true //This is a boolean
+// //Here is what I learned today, that could be logged into the console.
+// var studentName = "Samson" //This is a string
+// var pets = 1 //This is a integer
+// var funActivity = "I love cars"
+// var sexyMan = true //This is a boolean
 
-var studentID = studentName + pets + funActivity + sexyMan
+// var studentID = studentName + pets + funActivity + sexyMan
 
-//we console logged the studentname, number of pets, and a fun activity
+// //we console logged the studentname, number of pets, and a fun activity
 
-console.log(studentID)
+// console.log(studentID)
 
+// let ranNum = "0123456789"
 
-Math.floor(math.random())
+// if (ranNum === "0,1,2,3,4,5,6,7,8,9,0,10")
+//     {console.log (${ranNum})}
+
+// // Math.floor(math.random())
+
+const number = prompt("Enter a number 1-4")
+
+if (number == 1){
+    console.log ("yes");
+    }
+    else if (number == 2) {
+        console.log("heck yes!");
+    }
+    else {(number === 4);
+        console.log("heckyyy")
+    }
